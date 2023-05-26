a7ac0646c5226d4998f9dd4eeeb0127435a647ee
Helge Sverre <helge.sverre@gmail.com>
2023-05-26 22:13:15 +0200

    show debug message if debugmode, fix minor ui thing

--- resources/views/livewire/chatbot.blade.php
+++ resources/views/livewire/chatbot.blade.php
@@ -16,13 +16,13 @@
 
             @foreach($messages as $message)
 
-                @if($message["role"] == "system")
 
+                @if($message["role"] == "system" && $this->debug)
                     <div class="flex justify-end">
                         <div class="mb-4 flex">
                             <div class="flex-1 px-2">
                                 <div class="inline-block max-w-xl rounded-3xl p-4 bg-bob-yellow-light text-gray-900/80">
-                                    <div class="font-bold text-xs mb-1">Oppslag i kunnskapsdatabasen:</div>
+                                    <div class="font-bold text-xs mb-1">Debug</div>
                                     <span>{!! nl2br($message["content"]) !!}</span>
                                 </div>
                             </div>
@@ -46,7 +46,7 @@
                             </div>
                         </div>
                     </div>
-                @else
+                @elseif($message["role"] == "assistant")
                     <div class="flex justify-end">
                         <div class="mb-4 flex">
                             <div class="flex-1 px-2">
@@ -69,7 +69,7 @@
         <div class="h-12 bg-white border border-gray-200 shadow flex rounded-lg w-full bg-red-600 overflow-hidden">
             <div
                 wire:loading.delay.class.remove="hidden"
-                class="hidden absolute inset-0 bg-gray-400 z-50 flex items-center justify-center opacity-60"
+                class="hidden m-1 absolute inset-0 bg-gray-400 z-50 flex items-center justify-center opacity-60 rounded-lg"
             >
                 <x-heroicon-m-cog class="animate-spin h-6 w-6 text-gray-600"/>
             </div>
@@ -90,7 +90,7 @@
                     wire:loading.attr="disabled"
                     class="bg-blue-400 w-9 h-9 rounded-full flex justify-center items-center"
                 >
-                    <x-heroicon-m-paper-airplane class="w-5 h-5 text-white -rotate-45 animate-spin"/>
+                    <x-heroicon-m-paper-airplane class="w-5 h-5 text-white -rotate-45"/>
                 </button>
             </div>
         </div>
