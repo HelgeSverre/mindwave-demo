82f6bcba2072a0cfa4cb76e657cf50ba7e8b3ef5
Helge Sverre <helge.sverre@gmail.com>
2023-05-26 22:11:55 +0200

    favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,19 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mindwave Demo</title>
+    <title>Mindwave - Demo Application</title>
+
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset("/favicon/apple-touch-icon.png") }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset("/favicon/favicon-32x32.png") }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset("/favicon/favicon-16x16.png") }}">
+    <link rel="manifest" href="{{ asset("/favicon/site.webmanifest") }}">
+    <link rel="mask-icon" href="{{ asset("/favicon/safari-pinned-tab.svg") }}" color="#a66bb5">
+    <link rel="shortcut icon" href="{{ asset("/favicon/favicon.ico") }}">
+    <meta name="msapplication-TileColor" content="#aa6db8">
+    <meta name="msapplication-config" content="{{ asset("/favicon/browserconfig.xml") }}">
+    <meta name="theme-color" content="#ffffff">
+
 
     @vite(['resources/js/app.js', 'resources/css/app.css'])
     <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/intersect@3.x.x/dist/cdn.min.js"></script>
